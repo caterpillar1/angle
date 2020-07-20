d48d84a30f0bc71cb03cb2023edb61ecfb503d66
dxn <78287147@qq.com>
2020-07-20 13:58:07 +0800

    feat:node

---
+++ web/js/app.js
@@ -0,0 +1,81 @@
+$.loadingBig = '<i style="font-size: 32px;" class="layui-icon layui-icon-loading-1 layui-anim layui-anim-rotate layui-anim-loop"></i>';
+$.loadingMini = '<i class="layui-icon layui-icon-loading-1 layui-anim layui-anim-rotate layui-anim-loop"></i>';
+$.triggerList = [];
+//JavaScript代码区域
+layui.use('element', function(){
+    var element = layui.element;
+
+});
+
+layui.use('laydate', function(){
+    var laydate = layui.laydate;
+
+    //执行一个laydate实例
+    laydate.render({
+        elem: '.date-input' //指定元素
+    });
+
+    //执行一个laydate实例
+    laydate.render({
+        elem: '.date-range-input',
+        range: true
+    });
+});
+
+$('.admin-grid-buttons > button,.admin-pop-btn').click(function (e) {
+    var url = $(this).data('url');
+    var action = $(this).data('action');
+    if(action === 'delete') {
+        return true;
+    }
+    if(url) {
+        layer.open({
+            type: 2,
+            area: ['80%', '80%'],
+            title: ' ',
+            scrollbar:true,
+            shadeClose: true,
+            shade: 0.5,
+            maxmin: true, //开启最大化最小化按钮
+            content: url
+        });
+    }
+});
+
+$(".routes-check-all").siblings('.layui-form-checkbox').click(function () {
+    var c = $(this).siblings('.routes-check-all').prop("checked");
+    var i = $(this).parent().siblings('.layui-field-box').find('input:checkbox');
+    i.each(function () {
+        if($(this).attr('disabled') !== 'disabled') {
+            $(this).prop("checked", c);
+            if ($(this).prop("checked")) {
+                $(this).next().addClass('layui-form-checked');
+            } else {
+                $(this).next().removeClass('layui-form-checked');
+            }
+        }
+    });
+});
+
+$('.searchPage').keyup(function(){
+    var val = $(this).val();
+    // 查找当前选择的筛选条件
+    var input = $(this).parent().parent().siblings('.layui-form-item').find('.own-routes-list').find('input')
+    input.each(function(){
+        var str = $(this).val();
+        if(str.indexOf(val)>=0){
+            $(this).next().show();
+        }else{
+            $(this).next().hide();
+        }
+    });
+});
+
+var _hmt = _hmt || [];
+(function() {
+    var hm = document.createElement("script");
+    hm.src = "https://hm.baidu.com/hm.js?74de7f42b1a01a1ef0a7dd6adce1dddc";
+    var s = document.getElementsByTagName("script")[0];
+    s.parentNode.insertBefore(hm, s);
+})();
+
---
+++ web/js/site.js
@@ -0,0 +1,167 @@
+function refreshSite() {
+    $.ajax({
+        type:"POST",
+        dataType:"json",
+        data:$("#filter-form").serialize(),
+        url:"/site/main",
+        beforeSend:function(){
+            $(".admin-card-num").empty().html($.loadingBig);
+            $(".admin-card-total-num").empty().html($.loadingMini);
+            var main = $("#admin-site-main");
+            main.prev().show();
+            main.next().remove();
+        },
+        complete:function(){
+            $(".admin-card-num").children('i').remove();
+            $(".admin-card-total-num").children('i').remove();
+            $("#admin-site-main").prev().hide();
+        },
+        success:function(json){
+            $(".admin-card-income").html(json[1].dau_income);
+            $(".admin-card-payp").html(json[1].dau_payp);
+            $(".admin-card-new_p").html(json[1].new_p);
+            $(".admin-card-dau").html(json[1].dau);
+
+            $(".admin-card-total-income").html(json[0].dau_income);
+            $(".admin-card-total-payp").html(json[0].dau_payp);
+            $(".admin-card-total-new_p").html(json[0].new_p);
+            $(".admin-card-total-dau").html(json[0].dau);
+            refreshTable(json);
+        }
+    });
+}
+
+function refreshTable(json) {
+    layui.use('table', function(){
+        var table = layui.table;
+        //展示已知数据
+        table.render({
+            elem: '#admin-site-main',
+            toolbar: true,
+            cellMinWidth:'110',
+            cols: [[ //标题栏
+                {field: 'ymd', title: '日期', sort:true},
+                {field: 'active_num', title: '激活数', sort:true},
+                {field: 'new_dev_num', title: '新增账号设备数', sort:true,minWidth:150},
+                {field: 'new_p', title: '新增账号', sort:true},
+                {field: 'new_role', title: '新增角色', sort:true},
+                {field: 'new_pay', title: '新增充值', sort:true},
+                {field: 'new_payp', title: '新增充值人数', sort:true},
+                {field: 'new_pay_lv', title: '新增付费率', sort:true},
+                {field: 'new_pay_arpu', title: '新增付费ARPU', sort:true},
+                {field: 'new_arpu', title: '新增ARPU', sort:true},
+                {field: 'dau_income', title: '充值金额', sort:true},
+                {field: 'dau_payp', title: '充值人数', sort:true},
+                {field: 'dau_pay_lv', title: '付费渗透率', sort:true},
+                {field: 'pay_arpu', title: '付费ARPU', sort:true},
+                {field: 'dau_arpu', title: '登录ARPU', sort:true},
+                {field: 'dau', title: '登录人数', sort:true},
+                {field: 'online_avg', title: '平均在线', sort:true},
+                {field: 'online_max', title: '最高在线', sort:true},
+                {field: 'd1', title: '次日留存数', sort:true},
+                {field: 'l1', title: '次日留存率', sort:true},
+                {field: 'd3', title: '3日留存数', sort:true},
+                {field: 'l3', title: '3日留存率', sort:true},
+                {field: 'd7', title: '7日留存数', sort:true},
+                {field: 'l7', title: '7日留存率', sort:true},
+                {field: 'd30', title: '30日留存数', sort:true},
+                {field: 'l30', title: '30日留存率', sort:true}
+            ]]
+            ,data: json
+            ,skin: 'row' //表格风格
+            ,even: true
+            ,limit:json.length
+            ,height:500,
+            done: function(res, curr, count) {
+                refreshChart(this);
+            }
+        });
+    });
+}
+
+function refreshChart(table) {
+    var chart = echarts.init(document.getElementById('admin-chart-income'));
+    var xAxisData = [];
+    var seriesData = [];
+    var legendData = [];
+    var seriesDataTemp = {};
+    var tempCols = table.cols[0];
+    var cols = {};
+    for (var i in tempCols) {
+        cols[tempCols[i].field] = tempCols[i].title;
+        legendData.push(tempCols[i].title);
+    }
+    var data = table.data;
+    for (var k in data) {
+        var items = data[k];
+        if(items.ymd !== '汇总') {
+            xAxisData.push(items.ymd);
+            for(var i in items) {
+                if(seriesDataTemp[i] === undefined){
+                    seriesDataTemp[i] = [];
+                }
+                seriesDataTemp[i].push(items[i])
+            }
+        }
+    }
+    for (var key in seriesDataTemp) {
+        if(key === 'ymd') continue;
+        if(cols[key] === undefined) continue;
+        for (var i in seriesDataTemp[key]) {
+            var val = seriesDataTemp[key][i].toString();
+            val = val.replace('%','');
+            seriesDataTemp[key][i] = val
+        }
+        seriesData.push({
+            name:cols[key],
+            type:'line',
+            data:seriesDataTemp[key]
+        });
+    }
+    option = {
+        tooltip : {
+            trigger: 'axis',
+            axisPointer: {
+                type : 'shadow'
+            }
+        },
+        legend: {
+            type: 'scroll',
+            orient: 'vertical',
+            right: 10,
+            top: 20,
+            bottom: 20,
+            data: legendData
+        },
+        grid: {
+            left: '3%',
+            right: '150px',
+            bottom: '3%',
+            containLabel: true
+        },
+        xAxis : [
+            {
+                type : 'category',
+                data : xAxisData,
+                axisTick: {
+                    alignWithLabel: true
+                }
+            }
+        ],
+        yAxis : [
+            {
+                type : 'value'
+            }
+        ],
+        series : seriesData,
+        animationEasing: 'elasticOut'
+
+    };
+    chart.clear();
+    chart.setOption(option);
+}
+var obj = $("body");
+obj.on('refresh',function () {
+    refreshSite();
+});
+$.triggerList.push(obj);
